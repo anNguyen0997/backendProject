90c6e37672871e5dab3eecfe4fd132f413a9f4e0
An Nguyen <roryn4545@gmail.com>
2022-10-05 15:18:56 -0500

    missing async

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/login', (req, res)=> {                                                            // Renders Login Page
+app.get('/login', async(req, res)=> {                                                            // Renders Login Page
     res.render("login")
 
     let loggedUsers = await users.findOne({
