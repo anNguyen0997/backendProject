4c9e3e227e64a41a8edeb70d8c484e811c65b07d
WesWo1f <67709783+WesWo1f@users.noreply.github.com>
2022-10-07 16:07:29 -0400

    FP-9 edgecases for names username and password

--- server.js
+++ server.js
@@ -22,7 +22,48 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const bcrypt = require('bcrypt');                                                           // Imports package for enrypting passwords
 const saltRounds = 10;
 //------------------------------------------------------------------------------------------------------------------------
-
+function lettersAndNumbersCheck(word){
+    //return /^[A-Za-z0-9]*$/.test(str);
+    if(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/.test(word)){
+        console.log("this password contains both numbers and letters 8min 20max ")
+        return true 
+    }
+    else{
+        console.log("password was not good")
+        return false
+    }
+    //^(?=.*[a-zA-Z])(?=.*[0-9])
+}
+function lengthCheck(item, minLength, maxLength){                                           // checks the length of what is passed in
+    let itemLength = item.length;
+    if(itemLength < minLength){
+        console.log(item+" is too short")
+        return false
+    }
+    else if(itemLength > maxLength){
+        console.log(item+" is too long")
+        return false
+    }
+    else{
+        return true
+    }
+}
+function lettersOnly(word){
+    if (!/[^a-zA-Z]/.test(word)){
+        return true
+    }
+    else{
+        return false
+    }
+}
+function validateEmail(mail){
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)&&lengthCheck(mail,5,30)==true){          //checks to make sure email is valid and long enough
+        return (true)
+    }
+    else{
+        return (false)
+    }
+}
 
 //----------------------------------------------------------
 function getStats(){
@@ -50,26 +91,38 @@ app.get('/register', (req, res)=> {                                             
 })
 //----------------------------------------------------------
 app.post('/registerUser', async (req, res) => {                                              // Creates user in users table
-    
     let userExists = await users.findAll({
         where: {
             username: req.body.username
         }
     })
-
     if (Object.keys(userExists).length != 0) {                                               // Checks if username exist
         res.render('register',{msg:'User already exist'})
     }
     else {
-        
         let userExists = await users.findAll({
             where: {
                 email: req.body.email
             }
         })
-        
-        if (Object.keys(userExists).length != 0) {                                            // Checks if email exist
-            res.render('register',{msg:'Email already exist'})
+       
+        if(Object.keys(userExists).length != 0 || validateEmail(req.body.email)==false) {      // Checks if email exist & checks to see if it is a valid email
+            if(validateEmail(req.body.email)==false){
+                res.render('register',{msg:'Email must be > 5 char < 24 char long and have @ symbol.'})}
+            else{
+                res.render('register',{msg:'Email already exist'})}
+        }
+        else if(lettersOnly(req.body.firstname)==false||lettersOnly(req.body.lastname)==false||lettersOnly(req.body.username)==false){      
+            res.render('register',{msg:"Use only letters in first name,last name and username"})
+        }
+        else if(lengthCheck(req.body.firstname)==false||lengthCheck(req.body.lastname)==false||lengthCheck(req.body.username)==false){
+            res.render('register',{msg:"First name,last name and username must be between 2 and 15."})
+        }
+        else if(lengthCheck(req.body.password,8,20)==false){ //password length check min 8 max 20
+            res.render('register',{msg:"Password must be greater than characters 8 and less than 20."})
+        }
+        else if(lettersAndNumbersCheck(req.body.password)==false){
+            res.render('register',{msg:"Password must contain both letters and numbers."})
         }
         else {
             const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)                   // Hashes user password for database
@@ -85,7 +138,6 @@ app.post('/registerUser', async (req, res) => {                                 
             userstats['username'] = req.body.username
         
             await stats.create(userstats)                                                   // Creates instance in stats table
-    
     
             res.redirect('/login')                                                          // redirects to /login
         }   
